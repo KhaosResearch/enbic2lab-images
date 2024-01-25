c204273bde911ab2476e0eb3eebc628ce002131f
ireneeromero <ireero99@gmail.com>
2024-01-25 14:38:53 +0100

    update component

--- components/air/DataControlSelection/main.py
+++ components/air/DataControlSelection/main.py
@@ -1,7 +1,7 @@
 import argparse
 import pandas as pd
 
-def main(filepath_metrics:str,filepath_pandas:str,filepath_mean:str, output: str, delimiter: str = ";") -> None:
+def main(filepath_metrics:str,filepath_mean:str,filepath_pandas:str, output: str, delimiter: str = ";") -> None:
     """
     Select the best CSV by metrics comparation.
     """
@@ -17,27 +17,33 @@ def main(filepath_metrics:str,filepath_pandas:str,filepath_mean:str, output: str
     dict_ocurrence = dict()
     for index, row in df.iterrows():
         dict_ocurrence[index] = list_best.count(index)
-
     ocurrence_values = list(dict_ocurrence.values())
     max_ocurrence = max(ocurrence_values)
-    count_max_ocurrence = ocurrence_values.count(max_ocurrence)
 
+    count_max_ocurrence = ocurrence_values.count(max_ocurrence)
     if count_max_ocurrence == 1: 
         indx = ocurrence_values.index(max_ocurrence)
-        best_file = "/mnt/shared/"+ df['filename'].iloc[indx] 
+        if(indx==0):
+            best_file=filepath_mean
+        else:
+            best_file=filepath_pandas
     else: 
         max_r2 = 0
+        indx=0
         for elem in dict_ocurrence:
             if dict_ocurrence[elem] == max_ocurrence:
                 if df['r2_mean'].iloc[elem] > max_r2:
                     max_r2 = df['r2_mean'].iloc[elem]
-                    best_file ="/mnt/shared/"+ df['filename'].iloc[elem] 
-    print(best_file)
+                    if(indx==0):
+                        best_file=filepath_mean
+                    else:
+                        best_file=filepath_pandas
+                    indx+=1
     if best_file == filepath_pandas:
         df_final = pd.read_csv(filepath_pandas, sep=delimiter)
     elif best_file == filepath_mean:
         df_final = pd.read_csv(filepath_mean, sep=delimiter)
-
+    print(best_file)
     df_final.to_csv(output, sep=delimiter, index=None)
 
 if __name__ == "__main__":
@@ -81,8 +87,8 @@ if __name__ == "__main__":
 
     main(
         args.filepath_metrics,
-        args.filepath_pandas,
         args.filepath_mean,
+        args.filepath_pandas,
         args.output,
         args.delimiter
     )
